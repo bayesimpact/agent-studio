5334c95a7e067cb3a9dd095685726d0372de87ee
Alexis Jamet <alexis.jamet@gmail.com>
2025-12-24 12:48:21 +0100

    feat(backoffice): handle refresh token, add log-in/out, loading, notFound pages

--- apps/backoffice/src/App.tsx
+++ apps/backoffice/src/App.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Profile from './components/Profile';
 import { GuestRoute } from './routes/GuestRoute';
 import { HomeRoute } from './routes/HomeRoute';
+import { LoadingRoute } from './routes/LoadingRoute';
+import { LoginRoute } from './routes/LoginRoute';
+import { LogoutRoute } from './routes/LogoutRoute';
 import { NotFoundRoute } from './routes/NotFoundRoute';
 
 function ProtectedRoute({ children }: { children: ReactNode }) {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0()
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <LoadingRoute />
 
-  return isAuthenticated ? children : <Navigate to="/" />;
+  return isAuthenticated ? children : <Navigate to="/" />
 }
 
 function App() {
@@ -20,6 +23,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeRoute />} />
         <Route path="/guest" element={<GuestRoute />} />
+        <Route path="/login" element={<LoginRoute />} />
+        <Route path="/logout" element={<LogoutRoute />} />
         <Route
           path="/profile"
           element={
--- apps/backoffice/src/main.tsx
+++ apps/backoffice/src/main.tsx
@@ -10,10 +10,14 @@ createRoot(document.getElementById('root')!).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        scope: "openid profile email offline_access",
       }}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
     >
-    <App />
+      <App />
     </Auth0Provider>
   </StrictMode>,
 )
