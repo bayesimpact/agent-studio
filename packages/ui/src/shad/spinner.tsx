c63906e891e097ce737d1d1c7be27d366fb9e409
Alexis Jamet <alexis.jamet@gmail.com>
2026-01-02 10:31:45 +0100

    core(ui): add shad components

---
+++ packages/ui/src/shad/spinner.tsx
@@ -0,0 +1,17 @@
+
+import { cn } from "@repo/ui/utils"
+import { Loader2Icon } from "lucide-react"
+
+function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+  return (
+    // @ts-expect-error - lucide-react types are broken
+    <Loader2Icon
+      role="status"
+      aria-label="Loading"
+      className={cn("size-4 animate-spin", className)}
+      {...props}
+    />
+  )
+}
+
+export { Spinner }
