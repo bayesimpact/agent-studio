ff04aae81d458c30d12ffec5c7d25d41d02211cf
Did <didier.lafforgue@gmail.com>
2026-01-12 17:16:16 +0100

    chore: remove the web / prendresoin apps

--- apps/prendresoin/src/components/MessageTime.tsx
+++
@@ -1,3 +0,0 @@
-export function MessageTime({ timestamp }: { timestamp: Date }) {
-	return <p className="text-xs opacity-50">{timestamp.toLocaleTimeString()}</p>
-}
--- apps/prendresoin/src/components/chat/ChatInputSubmitButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@codegouvfr/react-dsfr/Button'
-
-export function ChatInputSubmitButton({ disabled, onClick }: { disabled: boolean; onClick: () => void }) {
-	return (
-		<Button disabled={disabled} onClick={onClick}>
-			Envoyer
-		</Button>
-	)
-}
