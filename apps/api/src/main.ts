e7cec15c6c13fa7e433b3a96b8517540e9045bc1
Did <didier.lafforgue@gmail.com>
2026-01-14 17:04:21 +0100

    chore: remove not required services + disable the function calling feature (for now)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
     origin: [
+      "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:3002",
       "https://caseai-connect-web.vercel.app",
       "https://connect.caseai.org",
-      "https://prendresoin-web.vercel.app",
-      "https://prendresoin-web.vercel.app",
-      "https://prendresoin.ft-qvr.connect.caseai.org",
-      "https://prendresoin.francetravail.connect.caseai.org/",
     ],
     credentials: true,
   })
--- apps/api/src/notion/notion.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common"
-import { NotionBeneficiaryService } from "./notion-beneficiary.service"
-import { NotionWorkshopService } from "./notion-workshop.service"
-
-@Module({
-  providers: [NotionWorkshopService, NotionBeneficiaryService],
-  exports: [NotionWorkshopService, NotionBeneficiaryService],
-})
-export class NotionModule {}
