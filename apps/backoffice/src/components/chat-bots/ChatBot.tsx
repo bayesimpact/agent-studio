fb7e5b7d76c50e2c367dfe2ede639bbf48902e88
Alexis Jamet <alexis.jamet@gmail.com>
2026-01-23 19:35:42 +0100

    feat(chatBot): show chat

---
+++ apps/backoffice/src/components/chat-bots/ChatBot.tsx
@@ -0,0 +1,79 @@
+import type { ChatBotDto } from "@caseai-connect/api-contracts"
+import { Button } from "@caseai-connect/ui/shad/button"
+import { Item, ItemContent, ItemHeader, ItemTitle } from "@caseai-connect/ui/shad/item"
+import { CirclePlusIcon, MicIcon, PaperclipIcon } from "lucide-react"
+import {
+  Chat,
+  ChatActions,
+  ChatBotMessage,
+  ChatContent,
+  ChatFooter,
+  ChatHeader,
+  ChatInput,
+  ChatSubmit,
+  ChatUserMessage,
+} from "../chat/Chat"
+import { DotsBackground } from "../DotsBackground"
+
+export function ChatBot({ chatBot }: { chatBot: ChatBotDto }) {
+  const messages = [
+    {
+      id: "1",
+      role: "bot",
+      content: "Hey, ask me anything!",
+    },
+    {
+      id: "2",
+      role: "user",
+      content: "What can you do?",
+    },
+  ]
+
+  const handleSubmit = (_message: string) => {
+    // TODO:
+  }
+  return (
+    <div className="p-6 flex flex-col gap-6 flex-1">
+      <Item variant="outline">
+        <ItemHeader>
+          <ItemTitle>Default Prompt</ItemTitle>
+        </ItemHeader>
+        <ItemContent>{chatBot.defaultPrompt}</ItemContent>
+      </Item>
+
+      <DotsBackground className="p-10">
+        <Chat>
+          <ChatHeader />
+          <ChatContent>
+            {messages.map((msg) =>
+              msg.role === "bot" ? (
+                <ChatBotMessage key={msg.id}>{msg.content}</ChatBotMessage>
+              ) : (
+                <ChatUserMessage key={msg.id}>{msg.content}</ChatUserMessage>
+              ),
+            )}
+          </ChatContent>
+
+          <ChatFooter onMessageSubmit={handleSubmit}>
+            <ChatInput placeholder="Ask a question..." className="resize-none" />
+
+            <ChatActions>
+              <div className="flex-1 justify-start flex gap-1">
+                <Button variant="secondary">
+                  <CirclePlusIcon />
+                </Button>
+                <Button variant="ghost">
+                  <PaperclipIcon />
+                </Button>
+                <Button variant="ghost">
+                  <MicIcon />
+                </Button>
+              </div>
+              <ChatSubmit variant="ghost" />
+            </ChatActions>
+          </ChatFooter>
+        </Chat>
+      </DotsBackground>
+    </div>
+  )
+}
--- apps/backoffice/src/routes/ChatBotRoute.tsx
+++ apps/backoffice/src/routes/ChatBotRoute.tsx
@@ -1,19 +1,22 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { ChatBot } from "@/components/chat-bots/ChatBot"
 import { useSidebarLayout } from "@/components/layouts/sidebar/context"
 import { selectCurrentChatBot } from "@/features/chat-bots/chat-bots.selectors"
 import { useAppSelector } from "@/store/hooks"
+import { NotFoundRoute } from "./NotFoundRoute"
 
 export function ChatBotRoute() {
   const { chatBotId, projectId } = useParams()
   const chatBot = useAppSelector(selectCurrentChatBot({ projectId, chatBotId }))
 
   const { setHeaderTitle } = useSidebarLayout()
-  const headerTitle = chatBot ? `Chat Bot - ${chatBot.name}` : "Chat Bot"
+  const headerTitle = chatBot ? chatBot.name : "Chat Bot"
 
   useEffect(() => {
     setHeaderTitle(headerTitle)
   }, [headerTitle, setHeaderTitle])
 
-  return <div>TODO: Chat Bot {JSON.stringify(chatBot)}</div>
+  if (!chatBot) return <NotFoundRoute />
+  return <ChatBot chatBot={chatBot} />
 }
