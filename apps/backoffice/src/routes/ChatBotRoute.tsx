dcbf595466ed4dfdcc3542c7abba5811e538b162
Alexis Jamet <alexis.jamet@gmail.com>
2026-01-23 17:05:52 +0100

    fix(chatbot): header title

--- apps/backoffice/src/routes/ChatBotRoute.tsx
+++ apps/backoffice/src/routes/ChatBotRoute.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react"
 import { useLoaderData } from "react-router-dom"
+import { useSidebarLayout } from "@/components/layouts/sidebar/context"
 import type { ChatBotLoaderData } from "./loaders/load-chat-bot"
 
 export function ChatBotRoute() {
-  const data = useLoaderData<ChatBotLoaderData>()
+  const chatBot = useLoaderData<ChatBotLoaderData>()
 
-  return <div>TODO: Chat Bot {JSON.stringify(data)}</div>
+  const { setHeaderTitle } = useSidebarLayout()
+  const headerTitle = chatBot ? `Chat Bot - ${chatBot.name}` : "Chat Bot"
+
+  useEffect(() => {
+    setHeaderTitle(headerTitle)
+  }, [headerTitle, setHeaderTitle])
+
+  return <div>TODO: Chat Bot {JSON.stringify(chatBot)}</div>
 }
