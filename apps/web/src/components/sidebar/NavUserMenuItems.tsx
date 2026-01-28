8395e20846032297825858a9c49cadb95b8d65b7
Alexis Jamet <alexis.jamet@gmail.com>
2026-01-28 17:48:28 +0100

    feat(navUserMenu): clean compontents

--- apps/web/src/components/sidebar/NavUserMenuItems.tsx
+++ apps/web/src/components/sidebar/NavUserMenuItems.tsx
@@ -1,45 +1,28 @@
-import {
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-} from "@caseai-connect/ui/shad/dropdown-menu"
-import { BadgeCheckIcon, BellIcon, CreditCardIcon, LogOutIcon, SparklesIcon } from "lucide-react"
+import { DropdownMenuItem, DropdownMenuSeparator } from "@caseai-connect/ui/shad/dropdown-menu"
+import { LogOutIcon } from "lucide-react"
 import { useTranslation } from "react-i18next"
 import { useAuthHandler } from "@/hooks/use-auth-handler"
 
 export function NavUserMenuItems() {
+  return (
+    <>
+      <ChangeLanguage />
+
+      <DropdownMenuSeparator />
+
+      <LogOutButton />
+    </>
+  )
+}
+
+function LogOutButton() {
   const { t } = useTranslation("user", { keyPrefix: "menu" })
   const { handleLogOut } = useAuthHandler()
   return (
-    <>
-      <DropdownMenuGroup>
-        <ChangeLanguage />
-        <DropdownMenuItem>
-          <SparklesIcon />
-          {t("upgrade")}
-        </DropdownMenuItem>
-      </DropdownMenuGroup>
-      <DropdownMenuSeparator />
-      <DropdownMenuGroup>
-        <DropdownMenuItem>
-          <BadgeCheckIcon />
-          {t("account")}
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <CreditCardIcon />
-          {t("billing")}
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <BellIcon />
-          {t("notifications")}
-        </DropdownMenuItem>
-      </DropdownMenuGroup>
-      <DropdownMenuSeparator />
-      <DropdownMenuItem onSelect={handleLogOut}>
-        <LogOutIcon />
-        {t("logout")}
-      </DropdownMenuItem>
-    </>
+    <DropdownMenuItem onSelect={handleLogOut}>
+      <LogOutIcon />
+      {t("logout")}
+    </DropdownMenuItem>
   )
 }
 
