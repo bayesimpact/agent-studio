cb0e6fb58f5f103464c1d5779cd90beeb7fb3aa3
Alexis Jamet <alexis.jamet@gmail.com>
2026-01-28 16:23:42 +0100

    fix(hook): remove useless useMobile hook

--- packages/ui/src/shad/sidebar.tsx
+++ packages/ui/src/shad/sidebar.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { useIsMobile } from "@caseai-connect/ui/hooks/use-mobile"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 import { PanelLeftIcon } from "lucide-react"
 import * as React from "react"
+import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "../lib/utils"
 import { Button } from "./button"
 import { Input } from "./input"
