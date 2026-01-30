81cb3d4c14d754e166bb1109cd6c3c8202ff3d8d
Alexis Jamet <alexis.jamet@gmail.com>
2026-01-30 14:20:09 +0100

    feat(app): add routes

---
+++ apps/web/src/routes/app/routes.tsx
@@ -0,0 +1,61 @@
+import { ChatBotRoute } from "../ChatBotRoute"
+import { DashboardRoute } from "../DashboardRoute"
+import { buildAppPath, RouteNames } from "../helpers"
+import { ChatBotLoader } from "../loaders/ChatBotLoader"
+import { ChatBotsLoader } from "../loaders/ChatBotsLoader"
+import { ChatSessionLoader } from "../loaders/ChatSessionLoader"
+import { ProjectLoader } from "../loaders/ProjectLoader"
+import { ProjectsLoader } from "../loaders/ProjectsLoader"
+import { UserHoc } from "../loaders/UserHoc"
+import { OrganizationsLoader } from "../OrganizationsLoader"
+import { ProjectRoute } from "../ProjectRoute"
+import { ProtectedRoute } from "../ProtectedRoute"
+
+export const appRoutes = [
+  {
+    path: buildAppPath(RouteNames.ORGANIZATION_DASHBOARD),
+    element: (
+      <ProtectedRoute>
+        <UserHoc>
+          {(user) => (
+            <OrganizationsLoader>
+              {(organizationId) => (
+                <ProjectsLoader organizationId={organizationId}>
+                  <DashboardRoute user={user} />
+                </ProjectsLoader>
+              )}
+            </OrganizationsLoader>
+          )}
+        </UserHoc>
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        index: true,
+        element: <div>TODO: Dashboard App</div>,
+      },
+      {
+        path: buildAppPath(RouteNames.PROJECT),
+        element: (
+          <ProjectLoader>
+            <ChatBotsLoader>
+              <ProjectRoute />
+            </ChatBotsLoader>
+          </ProjectLoader>
+        ),
+        children: [
+          {
+            path: buildAppPath(RouteNames.CHAT_BOT),
+            element: (
+              <ChatBotLoader>
+                <ChatSessionLoader>
+                  <ChatBotRoute />
+                </ChatSessionLoader>
+              </ChatBotLoader>
+            ),
+          },
+        ],
+      },
+    ],
+  },
+]
