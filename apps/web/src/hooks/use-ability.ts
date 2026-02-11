6faa68ea83a6c1a9ddeee8d21456a0221bae06c6
Alexis Jamet <alexis.jamet@gmail.com>
2026-02-11 16:52:14 +0100

    fix(AgentMessageFeedback): test

--- apps/web/src/hooks/use-ability.ts
+++ apps/web/src/hooks/use-ability.ts
@@ -2,8 +2,6 @@ import { useMemo } from "react"
 import { selectIsAdmin, selectIsAdminInterface } from "@/features/auth/auth.selectors"
 import { useAppSelector } from "@/store/hooks"
 
-// FIXME: This is a temporary hook until we implement a full ability system
-// TODO: should be replaced with a proper ability system like CASL
 export function useAbility() {
   const isAdmin = useAppSelector(selectIsAdmin)
   const isAdminInterface = useAppSelector(selectIsAdminInterface)
