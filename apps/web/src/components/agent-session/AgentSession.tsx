79b4a0ab5fcb85a2d0a40b873e323ed3896bb102
Alexis Jamet <alexis.jamet@gmail.com>
2026-02-27 17:13:54 +0100

    refactor(AgentSession): cleaner code

--- apps/web/src/components/agent-session/AgentSession.tsx
+++ apps/web/src/components/agent-session/AgentSession.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@caseai-connect/ui/shad/button"
-import { CirclePlusIcon, FileCheckIcon, XIcon } from "lucide-react"
+import { FileCheckIcon, XIcon } from "lucide-react"
 import { useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
 import type {
@@ -8,8 +8,6 @@ import type {
 } from "@/features/agent-sessions/agent-sessions.models"
 import { selectStreaming } from "@/features/agent-sessions/agent-sessions.selectors"
 import { sendMessage } from "@/features/agent-sessions/agent-sessions.thunks"
-import { selectCurrentOrganizationId } from "@/features/organizations/organizations.selectors"
-import { selectCurrentProjectId } from "@/features/projects/projects.selectors"
 import { useScrollToEnd } from "@/hooks/use-scroll-to-end"
 import { useAppDispatch, useAppSelector } from "@/store/hooks"
 import { Dictaphone } from "../agent/actions/Dictaphone"
@@ -36,38 +34,7 @@ export function AgentSession({
   session: AgentSessionType
   messages: AgentSessionMessageType[]
 }) {
-  const { t } = useTranslation()
-  const dispatch = useAppDispatch()
   const isStreaming = useAppSelector(selectStreaming)
-  const organizationId = useAppSelector(selectCurrentOrganizationId)
-  const projectId = useAppSelector(selectCurrentProjectId)
-
-  const [file, setFile] = useState<File>()
-
-  const chatEndRef = useRef<HTMLDivElement>(null)
-  const scrollToEnd = useScrollToEnd(chatEndRef)
-
-  useEffect(() => {
-    if (isStreaming) return
-    scrollToEnd()
-  }, [isStreaming, scrollToEnd])
-
-  if (!organizationId || !projectId) return null
-
-  const handleAttachDocument = (file: File) => {
-    setFile(file)
-  }
-
-  const handleUnattachDocument = () => {
-    setFile(undefined)
-  }
-
-  const handleSubmit = (message: string) => {
-    const trimedMessage = message.trim()
-    if (isStreaming || !trimedMessage) return
-    void dispatch(sendMessage({ content: trimedMessage, file }))
-    handleUnattachDocument()
-  }
 
   if (isAdminInterface)
     return (
@@ -77,92 +44,97 @@ export function AgentSession({
             <ChatHeader>
               <TraceUrlOpener traceUrl={session.traceUrl} />
             </ChatHeader>
-            <ChatContent>
-              {messages?.map((message) => (
-                <AgentSessionMessage key={message.id} message={message} />
-              ))}
-              <div ref={chatEndRef} />
-            </ChatContent>
 
-            <ChatFooter focus={!isStreaming} onMessageSubmit={handleSubmit}>
-              <ChatInput
-                placeholder={t("agentSession:chat.placeholder")}
-                className="resize-none"
-                disabled={isStreaming || !session}
-              />
+            <Messages messages={messages} isStreaming={isStreaming} />
 
-              <ChatActions>
-                <div className="flex-1 justify-start flex gap-1">
-                  <Button variant="secondary" disabled={isStreaming || !session}>
-                    <CirclePlusIcon />
-                  </Button>
-
-                  <div className="flex items-center gap-1">
-                    <AttachDocument
-                      onAttach={handleAttachDocument}
-                      disabled={isStreaming || !session}
-                    />
-                    {file && (
-                      <Button variant="default" onClick={handleUnattachDocument}>
-                        <FileCheckIcon className="size-4" /> {file?.name}
-                        <XIcon className="size-4" />
-                      </Button>
-                    )}
-                  </div>
-
-                  <Dictaphone disabled={isStreaming || !session} />
-                </div>
-                <ChatSubmit variant="ghost" disabled={isStreaming || !session} />
-              </ChatActions>
-            </ChatFooter>
+            <Footer session={session} isStreaming={isStreaming} />
           </Chat>
         </DotsBackground>
       </div>
     )
+
   return (
     <div className="flex flex-1 items-center justify-center p-4">
       <div className="flex flex-col gap-6 flex-1 max-w-2/3">
         <Chat className="shadow-none">
-          <ChatContent>
-            {messages?.map((message) => (
-              <AgentSessionMessage key={message.id} message={message} />
-            ))}
-            <div ref={chatEndRef} />
-          </ChatContent>
+          <Messages messages={messages} isStreaming={isStreaming} />
 
-          <ChatFooter focus={!isStreaming} onMessageSubmit={handleSubmit}>
-            <ChatInput
-              placeholder={t("agentSession:chat.placeholder")}
-              className="resize-none"
-              disabled={isStreaming || !session}
-            />
-
-            <ChatActions>
-              <div className="flex-1 justify-start items-center flex gap-1">
-                <Button variant="secondary" disabled={isStreaming || !session}>
-                  <CirclePlusIcon />
-                </Button>
-
-                <div className="flex items-center gap-1">
-                  <AttachDocument
-                    onAttach={handleAttachDocument}
-                    disabled={isStreaming || !session}
-                  />
-                  {file && (
-                    <Button variant="default" onClick={handleUnattachDocument}>
-                      <FileCheckIcon className="size-4" /> {file?.name}
-                      <XIcon className="size-4" />
-                    </Button>
-                  )}
-                </div>
-
-                <Dictaphone disabled={isStreaming || !session} />
-              </div>
-              <ChatSubmit variant="ghost" disabled={isStreaming || !session} />
-            </ChatActions>
-          </ChatFooter>
+          <Footer session={session} isStreaming={isStreaming} />
         </Chat>
       </div>
     </div>
+  )
+}
+
+function Messages({
+  messages,
+  isStreaming,
+}: {
+  messages: AgentSessionMessageType[]
+  isStreaming: boolean
+}) {
+  const chatEndRef = useRef<HTMLDivElement>(null)
+  const scrollToEnd = useScrollToEnd(chatEndRef)
+
+  useEffect(() => {
+    if (isStreaming) return
+    scrollToEnd()
+  }, [isStreaming, scrollToEnd])
+
+  return (
+    <ChatContent>
+      {messages?.map((message) => (
+        <AgentSessionMessage key={message.id} message={message} />
+      ))}
+      <div ref={chatEndRef} />
+    </ChatContent>
+  )
+}
+
+function Footer({ session, isStreaming }: { session: AgentSessionType; isStreaming: boolean }) {
+  const { t } = useTranslation()
+  const dispatch = useAppDispatch()
+
+  const [file, setFile] = useState<File>()
+
+  const handleAttachDocument = (attachedFile: File) => setFile(attachedFile)
+  const handleUnattachDocument = () => setFile(undefined)
+
+  const handleSubmit = (message: string) => {
+    const trimmedMessage = message.trim()
+    if (isStreaming || !trimmedMessage) return
+    void dispatch(sendMessage({ content: trimmedMessage, file }))
+    handleUnattachDocument()
+  }
+
+  return (
+    <ChatFooter focus={!isStreaming} onMessageSubmit={handleSubmit}>
+      <ChatInput
+        placeholder={t("agentSession:chat.placeholder")}
+        className="resize-none"
+        disabled={isStreaming || !session}
+      />
+
+      <ChatActions>
+        <div className="flex-1 justify-start flex gap-1">
+          {/* <Button variant="secondary" disabled={isStreaming || !session}>
+            <CirclePlusIcon />
+          </Button> */}
+
+          <div className="flex items-center gap-1">
+            <AttachDocument onAttach={handleAttachDocument} disabled={isStreaming || !session} />
+            {file && (
+              <Button variant="default" onClick={handleUnattachDocument}>
+                <FileCheckIcon className="size-4" /> {file?.name}
+                <XIcon className="size-4" />
+              </Button>
+            )}
+          </div>
+
+          <Dictaphone disabled={isStreaming || !session} />
+        </div>
+        <ChatSubmit variant="ghost" disabled={isStreaming || !session} />
+      </ChatActions>
+    </ChatFooter>
   )
 }
--- apps/web/src/components/document/UploadDocumentButton.tsx
+++ apps/web/src/components/document/UploadDocumentButton.tsx
@@ -4,8 +4,9 @@ import { BasicUploader } from "../FileUploader"
 
 export function UploadDocumentButton() {
   const dispatch = useAppDispatch()
-  const handleProcessFile = async ({ file }: { file: File }) => {
-    dispatch(uploadDocument({ file, sourceType: "project" }))
-  }
-  return <BasicUploader processFile={handleProcessFile} />
+  return (
+    <BasicUploader
+      processFile={({ file }) => dispatch(uploadDocument({ file, sourceType: "project" }))}
+    />
+  )
 }
