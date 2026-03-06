769f71399cc2e937f7068cccafc17b8af9f82244
Alexis Jamet <alexis.jamet@gmail.com>
2026-03-06 13:19:10 +0100

    refactor(streaming): clean code

--- apps/api/src/domains/agents/form-agent-sessions/form-agent-sessions.service.ts
+++ apps/api/src/domains/agents/form-agent-sessions/form-agent-sessions.service.ts
@@ -2,16 +2,21 @@ import { Inject, Injectable } from "@nestjs/common"
 import { InjectRepository } from "@nestjs/typeorm"
 import type { Repository } from "typeorm"
 import { v4 } from "uuid"
+import { z } from "zod"
 import { ConnectRepository } from "@/common/entities/connect-repository"
 import type { RequiredConnectScope } from "@/common/entities/connect-required-fields"
 import type { LLMProvider } from "@/common/interfaces/llm-provider.interface"
+import type { Agent } from "@/domains/agents/agent.entity"
 import { FILE_STORAGE_SERVICE } from "@/domains/documents/storage/file-storage.interface"
 import { ServiceWithLLM } from "@/external/llm"
 import type { BaseAgentSessionType } from "../base-agent-sessions/base-agent-sessions.types"
+import { fillFormTool } from "../shared/agent-session-messages/streaming/tools/fill-form.tool"
 import { FormAgentSession } from "./form-agent-session.entity"
 
 @Injectable()
 export class FormAgentSessionsService extends ServiceWithLLM {
+  private readonly sessionConnectRepository: ConnectRepository<FormAgentSession>
+  private readonly formAgentSessionRepository: Repository<FormAgentSession>
   constructor(
     @InjectRepository(FormAgentSession)
     formAgentSessionRepository: Repository<FormAgentSession>,
@@ -22,13 +27,12 @@ export class FormAgentSessionsService extends ServiceWithLLM {
     vertexLlmProvider: LLMProvider,
   ) {
     super(mockLlmProvider, vertexLlmProvider)
+    this.formAgentSessionRepository = formAgentSessionRepository
     this.sessionConnectRepository = new ConnectRepository(
       formAgentSessionRepository,
       "formAgentSession",
     )
   }
-
-  private readonly sessionConnectRepository: ConnectRepository<FormAgentSession>
 
   async listSessions({
     connectScope,
@@ -81,4 +85,101 @@ export class FormAgentSessionsService extends ServiceWithLLM {
     if (!session) return null
     return session
   }
+
+  buildFillFormTool({
+    agent,
+    sessionId,
+    sendClientEvent,
+  }: {
+    agent: Agent
+    sessionId: string
+    sendClientEvent: (event: MessageEvent) => void
+  }) {
+    const isValid = this.isValidSchema(agent.outputJsonSchema)
+    if (!isValid) {
+      console.error(`Invalid output JSON schema for agent ${agent.id}`)
+      return undefined
+    }
+
+    const agentOutputJsonSchema = agent.outputJsonSchema as AgentOutputJsonSchema
+    const inputSchema = this.buildZodSchema(agentOutputJsonSchema.properties)
+
+    const handleChange = async (value: Record<string, unknown>) => {
+      const updated = await this.formAgentSessionRepository.update(sessionId, {
+        // FIXME:
+        // @ts-expect-error
+        result: value,
+      })
+      if (!updated.affected) return
+
+      sendClientEvent({
+        data: JSON.stringify({
+          type: "form_update",
+          sessionId,
+        }),
+      } as MessageEvent)
+    }
+    return { fillForm: fillFormTool({ inputSchema, onExecute: handleChange }) }
+  }
+
+  // TODO: write a test for this method
+  private buildZodSchema(
+    properties: Record<string, { type: string; description: string }>,
+  ): z.ZodObject<any> {
+    const shape: Record<string, z.ZodTypeAny> = {}
+    for (const [key, value] of Object.entries(properties)) {
+      switch (value.type) {
+        case "string":
+          shape[key] = z.string().describe(value.description).optional()
+          break
+        case "number":
+          shape[key] = z.number().describe(value.description).optional()
+          break
+        case "boolean":
+          shape[key] = z.boolean().describe(value.description).optional()
+          break
+        default:
+          throw new Error(`Unsupported property type: ${value.type}`)
+      }
+    }
+
+    return z.object(shape).strict()
+  }
+
+  // TODO: write a test for this method
+  private isValidSchema(schema: Record<string, unknown> | null): schema is {
+    required: string[]
+    properties: Record<string, { type: string; description: string }>
+  } {
+    if (!schema) {
+      return false
+    }
+
+    if (
+      !Array.isArray(schema.required) ||
+      !schema.required.every((item) => typeof item === "string")
+    ) {
+      return false
+    }
+
+    if (
+      typeof schema.properties !== "object" ||
+      schema.properties === null ||
+      Array.isArray(schema.properties)
+    ) {
+      return false
+    }
+
+    return Object.values(schema.properties as Record<string, unknown>).every(
+      (prop) =>
+        typeof prop === "object" &&
+        prop !== null &&
+        typeof (prop as Record<string, unknown>).type === "string" &&
+        typeof (prop as Record<string, unknown>).description === "string",
+    )
+  }
+}
+type AgentOutputJsonSchema = {
+  required: string[]
+  properties: Record<string, { type: string; description: string }>
 }
