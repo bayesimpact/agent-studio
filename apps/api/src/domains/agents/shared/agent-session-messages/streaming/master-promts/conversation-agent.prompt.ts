16c63c1bc57eef23a5c890b4a5cfd5112b4fb329
Alexis Jamet <alexis.jamet@gmail.com>
2026-03-06 15:06:49 +0100

    fix(streaming): wrong sse message format

--- apps/api/src/domains/agents/shared/agent-session-messages/streaming/master-promts/conversation-agent.prompt.ts
+++ apps/api/src/domains/agents/shared/agent-session-messages/streaming/master-promts/conversation-agent.prompt.ts
@@ -6,9 +6,6 @@ Today's date: ${new Date().toLocaleDateString()}
 
 ${agent.defaultPrompt}
 
-# Attachment:
-If there is a file (image or pdf) attached to the user's chat message, answer the user's question or instruction reading the content of the file.
-
 Always answer in ${agent.locale}.
   `.trim()
 }
