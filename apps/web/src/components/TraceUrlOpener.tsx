75ee8f36c4db475f8a0e85b06e26b1516e175da9
Alexis Jamet <alexis.jamet@gmail.com>
2026-03-09 15:20:36 +0100

    feat(TraceUrl): show on pressing ctrl key and only for bayes members

--- apps/web/src/components/TraceUrlOpener.tsx
+++ apps/web/src/components/TraceUrlOpener.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@caseai-connect/ui/shad/button"
 import { ExternalLinkIcon } from "lucide-react"
+import { useEffect, useState } from "react"
+import { useAbility } from "@/hooks/use-ability"
 
 export function TraceUrlOpener({
   traceUrl,
@@ -8,7 +10,25 @@ export function TraceUrlOpener({
   traceUrl?: string
   buttonProps?: React.ComponentProps<typeof Button>
 }) {
-  if (!traceUrl) return null
+  const { isBayesMember } = useAbility()
+  const [isShown, setIsShown] = useState(false)
+
+  useEffect(() => {
+    if (!isBayesMember) return
+
+    function keyDownHandler(e: globalThis.KeyboardEvent) {
+      if (traceUrl && e.key === "Control") {
+        e.preventDefault()
+        setIsShown((state) => !state)
+      }
+    }
+    // Remove any existing listener before adding a new one
+    document.removeEventListener("keydown", keyDownHandler)
+    document.addEventListener("keydown", keyDownHandler)
+    return () => document.removeEventListener("keydown", keyDownHandler)
+  }, [traceUrl, isBayesMember])
+
+  if (!traceUrl || !isBayesMember || !isShown) return null
   return (
     <Button asChild variant="ghost" {...buttonProps}>
       <a href={traceUrl} className="cursor-pointer" target="_blank" rel="noreferrer">
--- apps/web/src/features/me/me.selectors.ts
+++ apps/web/src/features/me/me.selectors.ts
@@ -1,6 +1,7 @@
 import type { RootState } from "@/store"
 
 export const selectMe = (state: RootState) => state.me.data
-export const selectMeData = (state: RootState) => state.me.data
 export const selectMeStatus = (state: RootState) => state.me.data.status
 export const selectMeError = (state: RootState) => state.me.data.error
+export const selectIsBayesMember = (state: RootState): boolean =>
+  !!state.me.data?.value?.email.endsWith("@bayesimpact.org")
