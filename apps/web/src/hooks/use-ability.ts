178ad42af53a2db4e40d9c43f5038be7d4dde029
Alexis Jamet <alexis.jamet@gmail.com>
2026-03-09 16:29:10 +0100

    feat(isPremiumMember): use VITE_PREMIUM_EMAIL var

--- apps/web/src/hooks/use-ability.ts
+++ apps/web/src/hooks/use-ability.ts
@@ -1,14 +1,14 @@
 import { useMemo } from "react"
 import { selectIsAdmin, selectIsAdminInterface } from "@/features/auth/auth.selectors"
-import { selectIsBayesMember } from "@/features/me/me.selectors"
+import { selectIsPremiumMember } from "@/features/me/me.selectors"
 import { useAppSelector } from "@/store/hooks"
 
 export function useAbility() {
   const isAdmin = useAppSelector(selectIsAdmin)
   const isAdminInterface = useAppSelector(selectIsAdminInterface)
-  const isBayesMember = useAppSelector(selectIsBayesMember)
+  const isPremiumMember = useAppSelector(selectIsPremiumMember)
   return useMemo(
-    () => ({ isAdmin, isAdminInterface: isAdminInterface, isBayesMember }),
-    [isAdmin, isAdminInterface, isBayesMember],
+    () => ({ isAdmin, isAdminInterface: isAdminInterface, isPremiumMember }),
+    [isAdmin, isAdminInterface, isPremiumMember],
   )
 }
