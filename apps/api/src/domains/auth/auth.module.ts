55e08c6c850e5600523c2f6b15ee1ec75e331e97
Didier Lafforgue <didier.lafforgue@gmail.com>
2026-03-10 16:43:47 +0100

    feat: CLI to import users/org from a CSV file (#104)
    * feat: CLI to create users in Auth0, create org on the platform and ask them to change their password from a CSV file
    
    * chore: update the installation doc
    
    * feat: redirect to the studio namespace once logged in as an owner

--- apps/api/src/domains/auth/auth.module.ts
+++ apps/api/src/domains/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { PassportModule } from "@nestjs/passport"
 import { Auth0InvitationSenderService } from "./auth0-invitation-sender.service"
+import { Auth0UserProvisioningService } from "./auth0-user-provisioning.service"
 import { Auth0UserInfoService } from "./auth0-userinfo.service"
 import { INVITATION_SENDER } from "./invitation-sender.interface"
 import { JwtStrategy } from "./jwt.strategy"
@@ -11,11 +12,12 @@ import { JwtStrategy } from "./jwt.strategy"
   providers: [
     JwtStrategy,
     Auth0UserInfoService,
+    Auth0UserProvisioningService,
     {
       provide: INVITATION_SENDER,
       useClass: Auth0InvitationSenderService,
     },
   ],
-  exports: [PassportModule, Auth0UserInfoService, INVITATION_SENDER],
+  exports: [PassportModule, Auth0UserInfoService, Auth0UserProvisioningService, INVITATION_SENDER],
 })
 export class AuthModule {}
