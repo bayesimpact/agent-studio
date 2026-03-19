ee88a27e0866226c4207551c46fbbe9135434853
Did <didier.lafforgue@gmail.com>
2026-03-19 18:22:08 +0100

    chore: debug workers smoke tests in the GH CI

--- apps/api/src/config/typeorm.ts
+++ apps/api/src/config/typeorm.ts
@@ -13,13 +13,19 @@ if (process.env.DATABASE_HOST?.startsWith("/cloudsql")) {
   }
 }
 
+const databaseUrl = process.env.DATABASE_URL
+
 export const config: () => TypeOrmModuleOptions = () => ({
   type: "postgres",
-  host: `${process.env.DATABASE_HOST}`,
-  port: Number(`${process.env.DATABASE_PORT}`),
-  username: `${process.env.DATABASE_USERNAME}`,
-  password: `${process.env.DATABASE_PASSWORD}`,
-  database: `${process.env.DATABASE_NAME}`,
+  ...(databaseUrl
+    ? { url: databaseUrl }
+    : {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : undefined,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+      }),
   entities: [`${__dirname}/../**/*.entity.{js,ts}`],
   migrations: [`${__dirname}/../**/migrations/*.{js,ts}`],
   autoLoadEntities: true,
