040cf15148d4c7813ae09757ee6f5d137abf7800
Olivier Doucy <doucy.olivier@gmail.com>
2026-03-27 10:37:10 +0100

    chore(opentelemetry): remove console log

--- apps/api/src/external/llm/open-telemetry-init.ts
+++ apps/api/src/external/llm/open-telemetry-init.ts
@@ -1,11 +1,10 @@
 import "dotenv/config"
 import { NodeSDK } from "@opentelemetry/sdk-node"
-import { BatchSpanProcessor, ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base"
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base"
 import { LangfuseIntegrationExporter } from "@/external/langfuse/langfuse-integration-exporter"
 
 export const sdk = new NodeSDK({
   spanProcessors: [
-    new BatchSpanProcessor(new ConsoleSpanExporter()),
     new BatchSpanProcessor(
       new LangfuseIntegrationExporter({
         secretKey: process.env.LANGFUSE_SK,
