9cda3bf2f804e20833ee820d25f11135c7543b72
Did <didier.lafforgue@gmail.com>
2026-04-02 18:20:50 +0200

    chore: use a helper function convert DTOs into the final type

--- apps/web/src/features/analytics/external/analytics.api.ts
+++ apps/web/src/features/analytics/external/analytics.api.ts
@@ -1,10 +1,7 @@
-import { AnalyticsRoutes } from "@caseai-connect/api-contracts"
+import { type AnalyticsDailyPointDto, AnalyticsRoutes } from "@caseai-connect/api-contracts"
 import { getAxiosInstance } from "@/external/axios"
+import type { AnalyticsDailyPoint } from "../analytics.models"
 import type { IAnalyticsSpi } from "../analytics.spi"
-
-function dateRangeQueryParams(startAt: number, endAt: number) {
-  return { params: { startAt, endAt } }
-}
 
 export default {
   getConversationsPerDay: async ({ organizationId, projectId, startAt, endAt }) => {
@@ -13,7 +10,7 @@ export default {
       AnalyticsRoutes.getConversationsPerDay.getPath({ organizationId, projectId }),
       dateRangeQueryParams(startAt, endAt),
     )
-    return response.data.data
+    return toAnalyticsDailyPoints(response.data.data)
   },
   getAvgUserQuestionsPerSessionPerDay: async ({ organizationId, projectId, startAt, endAt }) => {
     const axios = getAxiosInstance()
@@ -23,6 +20,17 @@ export default {
       AnalyticsRoutes.getAvgUserQuestionsPerSessionPerDay.getPath({ organizationId, projectId }),
       dateRangeQueryParams(startAt, endAt),
     )
-    return response.data.data
+    return toAnalyticsDailyPoints(response.data.data)
   },
 } satisfies IAnalyticsSpi
+
+function dateRangeQueryParams(startAt: number, endAt: number) {
+  return { params: { startAt, endAt } }
+}
+
+function toAnalyticsDailyPoints(dtos: AnalyticsDailyPointDto[]): AnalyticsDailyPoint[] {
+  return dtos.map((dto) => ({
+    date: dto.date,
+    value: dto.value,
+  }))
+}
